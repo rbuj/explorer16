3aefc9ce86a06561125ccf3d84be4af0f177a03b
rbuj <robert.buj@gmail.com>
2018-01-18 11:46:58 +0100

    rm unused code

--- D10ToggleTimer1.X/leds.h
+++ D10ToggleTimer1.X/leds.h
@@ -19,29 +19,9 @@
 #define LEDS_H
 
 #define LEDs_LAT        LATA
-#define LED_D3_LAT      LATAbits.LATA0
-#define LED_D4_LAT      LATAbits.LATA1
-#define LED_D5_LAT      LATAbits.LATA2
-#define LED_D6_LAT      LATAbits.LATA3
-#define LED_D7_LAT      LATAbits.LATA4
-#define LED_D8_LAT      LATAbits.LATA5
-#define LED_D9_LAT      LATAbits.LATA6
-#define LED_D10_LAT     LATAbits.LATA7      // Overlaps with S5
-
 #define LEDs_TRIS       TRISA
-#define LED_D3_TRIS     TRISAbits.TRISA0
-#define LED_D4_TRIS     TRISAbits.TRISA1
-#define LED_D5_TRIS     TRISAbits.TRISA2
-#define LED_D6_TRIS     TRISAbits.TRISA3
-#define LED_D7_TRIS     TRISAbits.TRISA4
-#define LED_D8_TRIS     TRISAbits.TRISA5
-#define LED_D9_TRIS     TRISAbits.TRISA6
-#define LED_D10_TRIS    TRISAbits.TRISA7    // Overlaps with S5
-
-#define INPUT   1
-#define OUTPUT  0
-#define LED_ON  1
-#define LED_OFF 0
+#define LED_ON          1
+#define LED_OFF         0
 
 #include <xc.h>
 #include <stdbool.h> /* Includes true/false definition */
